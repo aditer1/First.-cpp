6bb7dcc78356b8ddc0562734f5ad18b843c77f1e
Ashutosh Vyas <152255944+aditer1@users.noreply.github.com>
2024-08-19 15:44:57 +0530

    Add files via upload

---
+++ First.cpp
@@ -0,0 +1,46 @@
+// #include used to add header 
+#include <iostream>
+//It's HEADER. It contains functions & commands 
+
+using namespace std;
+
+
+int main()
+{    //end statements with semicolon
+    cout << "Yooo, Chhoti! " <<endl ; // <<endl se line change kr skte h 
+    cout<< "04" <<endl ;
+ 
+ auto x = 1;
+ auto y = "hi";
+ auto z = 1.2 ; //auto is used to auto detect variable type
+ 
+   cout<<x <<endl;
+   cout <<y <<endl;
+   cout <<z <<endl;
+
+double shit = 24.44444 ; //double is used for decimals 
+   cout << shit <<endl<<endl<<endl;
+   
+   
+string ashu = "Game over "; //string & char are datatypes used for characters saving. 
+  cout <<ashu   <<endl;
+    
+bool myLove = true;  //bool datatype is used to tell true/false in a statement 
+bool yours = false ;
+  cout<< "My love is "<<myLove   <<endl ;
+  cout<< "Your love is "<<yours <<endl ; 
+  
+string name ; 
+  cout <<"Please enter Your Name" <<endl ;
+    cin >>name ; //cin is used to take input from the user 
+  cout<<"Your name is "<<name<<endl<<endl<<endl;
+  
+  
+int a, b ;
+     cout<<"Number a will be"<<endl ;cin>>a;
+     cout<<"Number b will be"<<endl; cin>>b;
+  cout<<"Sum of your numbers are "<<a+b<<endl; //to call the input 
+  
+
+  
+}
